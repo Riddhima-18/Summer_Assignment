6418af2e08eaeb1fabd50687969d6541f9280f31
Riddhima Pandey <riddhima.p06@gmail.com>
2026-06-05 23:28:36 +0530

    Reshapethematrix.java

--- WEEK_1/DAY_4/Reshapethematrix.java
+++ WEEK_1/DAY_4/Reshapethematrix.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int rows=mat.length;
+        int column=mat[0].length;
+        if (rows*column != r*c) {
+            return mat;
+        }
+        int[][] result = new int[r][c];
+        for (int i = 0; i < rows*column; i++) {
+            result[i/c][i%c] = mat[i/column][i%column];         
+        }
+        return result;
+    }
+}
