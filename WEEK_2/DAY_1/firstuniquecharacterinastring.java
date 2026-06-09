da2085ed6374c9b2ffd21ce33caa496457ec32be
Riddhima Pandey <riddhima.p06@gmail.com>
2026-06-09 23:09:25 +0530

    firstuniquecharacterinastring.java

--- WEEK_2/DAY_1/firstuniquecharacterinastring.java
+++ WEEK_2/DAY_1/firstuniquecharacterinastring.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int firstUniqChar(String s) {
+        char a[]=s.toCharArray();
+        int freq[]=new int[26];
+        for(int i=0;i<a.length;i++){
+            freq[a[i]-'a']++;
+        }
+        for(int i=0;i<a.length;i++){
+            if(freq[a[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
