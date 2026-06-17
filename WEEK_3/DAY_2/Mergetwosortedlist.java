53af393aa1beda42ad317024c3f2514873247cf6
Riddhima Pandey <riddhima.p06@gmail.com>
2026-06-17 23:33:21 +0530

    Update Mergetwosortedlist.java

--- WEEK_3/DAY_2/Mergetwosortedlist.java
+++ WEEK_3/DAY_2/Mergetwosortedlist.java
@@ -1,1 +1,26 @@
+public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+    ArrayList<Integer> list = new ArrayList<>();
+
+    while (list1 != null) {
+        list.add(list1.val);
+        list1 = list1.next;
+    }
+
+    while (list2 != null) {
+        list.add(list2.val);
+        list2 = list2.next;
+    }
+
+    Collections.sort(list);
+
+    ListNode dummy = new ListNode(-1);
+    ListNode curr = dummy;
+
+    for (int num : list) {
+        curr.next = new ListNode(num);
+        curr = curr.next;
+    }
+
+    return dummy.next;
+}
