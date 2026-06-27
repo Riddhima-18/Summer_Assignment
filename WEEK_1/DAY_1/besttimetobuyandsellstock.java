ed8dec020369e546285c4a662b5d6e7d33748f7f
Riddhima Pandey <riddhima.p06@gmail.com>
2026-06-27 14:21:03 +0530

    Update besttimetobuyandsellstock.java

--- WEEK_1/DAY_1/besttimetobuyandsellstock.java
+++ WEEK_1/DAY_1/besttimetobuyandsellstock.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int bestbuy = prices[0];
+        for(int i=1 ; i<prices.length ; i++){
+            if(prices[i] > bestbuy){
+                maxProfit = Math.max(maxProfit , prices[i] - bestbuy);
+            }
+            bestbuy = Math.min(bestbuy , prices[i]);
+        }
+        return maxProfit;        
+    }
+}
